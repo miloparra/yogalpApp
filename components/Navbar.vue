51a2397d73ced52ffe5e56d1fd0a0b9a36766b1f
miloparra <miloparra1531.mp@gmail.com>
2025-06-24 10:10:25 +0200

    navbar - add style and manage tab routes

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,18 @@
 <template>
-    <header class="w-full fixed top-0 left-0 z-50 bg-white shadow-md">
-        <nav class="max-w-5xl mx-auto flex justify-center items-center h-16 gap-8 text-gray-700 font-medium">
-            <a href="#hero" class="hover:text-blue-600 transition-colors">Accueil</a>
-            <a href="#about" class="hover:text-blue-600 transition-colors">À propos</a>
-            <a href="#projects" class="hover:text-blue-600 transition-colors">Projets</a>
-            <a href="#contact" class="hover:text-blue-600 transition-colors">Contact</a>
-        </nav>
+    <header class="w-full fixed top-0 left-0 z-50 bg-[#f6eaca] text-[#a26b24] shadow-md">
+        <div class="flex justify-between">
+            <div class="logo flex w-1/8 justify-center items-center">
+                YOGALP
+            </div>
+            <nav class="max-w-5xl mx-auto flex justify-center items-center h-16 gap-8 font-medium">
+                <a href="#about" class="hover:text-[#ffba88] transition-colors">Yogalp</a>
+                <a href="#sessions" class="hover:text-[#ffba88] transition-colors">Horaires</a>
+                <a href="#events" class="hover:text-[#ffba88] transition-colors">Evenements</a>
+                <a href="#contact" class="hover:text-[#ffba88] transition-colors">Contact</a>
+            </nav>
+            <div class="w-1/8">
+            </div>
+        </div>
+
     </header>
 </template>
