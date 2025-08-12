eae7a75eaa70896442d86bd346c597e74d0eedb4
miloparra <miloparra1531.mp@gmail.com>
2025-08-12 15:16:32 +0200

    about - update page layout

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -1,74 +1,28 @@
 <template>
-    <section id="about" class="min-h-[calc(100vh-96px)] scroll-mt-24 mt-24 py-20">
-        <div class="h-[calc(100vh-96px)] my-[-80px] grid grid-cols-3 grid-rows-3">
-            <!-- Case 1 -->
-            <img class="w-full h-full object-cover border-r-3 border-r-[#9b4222] border-b-3 border-b-[#DB6437]"
-                src="public/photo_yoga_ext.jpg" alt="">
-            <!-- Case 2 -->
-            <div class="p-6 border-b-3 border-b-[#DB6437]">
-                <div class="title h-1/3 flex items-start">
-                    <img class="w-30" src="public/Logotype_Black.png" alt="">
-                </div>
-                <div class="paragraph h-2/3 flex items-end text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae ipsum sit amet enim venenatis
-                    commodo id volutpat nulla. Pellentesque pretium ac mi quis rutrum. Nullam convallis ex vel elit
-                    pellentesque porta. Sed convallis neque sit amet est mollis, et hendrerit mauris varius. Maecenas
-                    sed vehicula erat, elementum congue neque.
-                </div>
-            </div>
-            <!-- Case 3 -->
-            <div class="case flex justify-center bg-[#9b4222] border-b-3 border-b-[#DB6437]">
-                <img class="w-1/2 object-cover" src="public/Logo_YA_SM.png" alt="">
-            </div>
-            <!-- Case 4 -->
-            <div class="case p-6 bg-[#9b4222] border-r-3 border-r-[#9b4222] text-amber-50">
-                <div class="title h-1/3 text-7xl">
-                    Yoga
-                </div>
-                <div class="paragraph h-2/3 flex items-end text-sm ml-40">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae ipsum sit amet enim
-                    venenatis
-                    commodo id volutpat nulla. Pellentesque pretium ac mi quis rutrum. Nullam convallis ex vel elit
-                    pellentesque porta.
-                </div>
-            </div>
-            <!-- Case 5 -->
-            <div class=""></div>
-            <!-- Case 6 -->
-            <div class="p-6">
-                <div class="title h-1/3 text-7xl">
-                    Julie
-                </div>
-                <div class="paragraph h-2/3 flex items-end text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae ipsum sit amet enim
-                    venenatis
-                    commodo id volutpat nulla. Pellentesque pretium ac mi quis rutrum. Nullam convallis ex vel elit
-                    pellentesque porta. Sed convallis neque sit amet est mollis, et hendrerit mauris varius.
-                    Maecenas
-                    sed vehicula erat, elementum congue neque.
-                </div>
-            </div>
-            <!-- Case 7 -->
-            <div class="p-6 flex border-r-3 border-r-[#9b4222]">
-                <div class="flex justify-center w-1/4">
-                    <div class="ml-30 rotate-270">
-                        <div class="flex justify-center text-4xl">
-                            HATHA
-                        </div>
-                        <div class="flex justify-center text-5xl">
-                            YOGA
-                        </div>
-                    </div>
-                </div>
-                <div class="paragraph w-3/4 flex items-center pr-7 text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae ipsum sit amet enim
-                    venenatis
-                    commodo id volutpat nulla. Pellentesque pretium ac mi quis rutrum. Nullam convallis ex vel elit
-                    pellentesque porta. Sed convallis neque sit amet est mollis, et hendrerit mauris varius.
-                </div>
-            </div>
-            <!-- Case 8/9 -->
-            <img class="col-span-2 w-full h-full object-cover" src="public/chaine_aravis.jpg" alt="">
+  <section id="about" class="scroll-mt-24 mt-24 py-20">
+    <div class="h-[calc(100vh-96px)] my-[-80px] grid grid-cols-3 relative overflow-hidden">
+      <!-- Image -->
+      <div class="col-span-2 relative">
+        <img
+          src="public/photo_yoga_ext.jpg"
+          alt="Yoga"
+          class="w-full h-full object-cover border-r-3 border-r-[#9b4222] border-b-3 border-b-[#DB6437]"
+        />
+        <div class="absolute bottom-4 left-4 text-white text-4xl">YOGA</div>
+      </div>
+
+      <!-- Texte -->
+      <div class="p-6 flex flex-col">
+        <div class="flex-1 flex justify-center items-center text-8xl">
+          Yogalp
         </div>
-    </section>
+        <div class="flex-2 flex justify-center items-center text-sm text-justify mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae ipsum sit amet enim venenatis commodo id
+          volutpat nulla. Pellentesque pretium ac mi quis rutrum. Nullam convallis ex vel elit pellentesque porta. Sed
+          convallis neque sit amet est mollis, et hendrerit mauris varius. Maecenas sed vehicula erat, elementum congue
+          neque.
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
