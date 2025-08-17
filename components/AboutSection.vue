588dfc8135d8bbc01a37cdee0a36574b557ca608
miloparra <miloparra1531.mp@gmail.com>
2025-08-17 21:05:33 +0200

    about - update page layout

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -1,22 +1,21 @@
 <template>
   <section id="about" class="scroll-mt-24 mt-24 py-20">
-    <div class="h-[calc(100vh-96px)] my-[-80px] grid grid-cols-3 relative overflow-hidden">
+    <div class="h-[calc(100vh-96px)] my-[-80px] grid grid-cols-3 overflow-hidden">
       <!-- Image -->
-      <div class="col-span-2 relative">
+      <div class="col-span-2 bg-[#DB6437]">
         <img
           src="public/photo_yoga_ext.jpg"
           alt="Yoga"
-          class="w-full h-full object-cover border-r-3 border-r-[#9b4222] border-b-3 border-b-[#DB6437]"
+          class="w-full h-full object-cover border-r-3 border-r-[#DB6437] border-b-3 border-b-[#DB6437] pr-6"
         />
-        <div class="absolute bottom-4 left-4 text-white text-4xl">YOGA</div>
       </div>
 
       <!-- Texte -->
-      <div class="p-6 flex flex-col">
-        <div class="flex-1 flex justify-center items-center text-8xl">
+      <div class="p-6 flex flex-col justify-center items-center">
+        <div class="w-3/5 flex justify-center items-center text-7xl">
           Yogalp
         </div>
-        <div class="flex-2 flex justify-center items-center text-sm text-justify mt-4">
+        <div class="w-3/5 flex justify-center items-center text-sm text-justify mt-15">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae ipsum sit amet enim venenatis commodo id
           volutpat nulla. Pellentesque pretium ac mi quis rutrum. Nullam convallis ex vel elit pellentesque porta. Sed
           convallis neque sit amet est mollis, et hendrerit mauris varius. Maecenas sed vehicula erat, elementum congue
