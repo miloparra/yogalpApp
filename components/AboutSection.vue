50bab1780df4ec9a485793f1b1f32b36d24cf01a
miloparra <miloparra1531.mp@gmail.com>
2025-08-18 12:34:28 +0200

    about - remove borders and add scale

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -1,17 +1,17 @@
 <template>
   <section id="about" class="scroll-mt-24 mt-24 py-20">
-    <div class="h-[calc(100vh-96px)] my-[-80px] grid grid-cols-3 overflow-hidden">
+    <div class="h-[calc(100vh-96px)] my-[-80px] grid grid-cols-3 overflow-hidden scale-90">
       <!-- Image -->
-      <div class="col-span-2 bg-[#DB6437]">
+      <div class="col-span-2">
         <img
           src="public/photo_yoga_ext.jpg"
           alt="Yoga"
-          class="w-full h-full object-cover border-r-3 border-r-[#DB6437] border-b-3 border-b-[#DB6437] pr-6"
+          class="w-full h-full object-cover"
         />
       </div>
 
       <!-- Texte -->
-      <div class="p-6 flex flex-col justify-center items-center">
+      <div class="p-6 flex flex-col justify-center items-center border-r-35 border-r-[#F27F53]">
         <div class="w-3/5 flex justify-center items-center text-7xl">
           Yogalp
         </div>
