01e64ca1754fbc4782c42ee937985d35e41370d7
miloparra <miloparra1531.mp@gmail.com>
2025-10-26 18:29:17 +0100

    home - add home section and update home page layout

---
+++ components/HomeSection.vue
@@ -0,0 +1,26 @@
+<template>
+    <section id="home" class="relative h-screen overflow-hidden">
+        <!-- Image -->
+        <div class="absolute inset-0">
+            <img
+            src="/IA_Yoga_Montagne.png"
+            alt="Yoga"
+            class="w-full h-full filter brightness-90 object-cover"
+            />
+        </div>
+
+        <!-- Texte -->
+        <div class="relative flex flex-col justify-center items-center gap-5 h-full text-white">
+            <div class="">
+                <img
+                src="/Logotype_White.png"
+                alt="Logotype"
+                class="w-120"
+                />
+            </div>
+            <div class="text-2xl font-medium">
+                L’harmonie au sommet
+            </div>
+        </div>
+    </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import BackgroundParallax from '~/components/BackgroundParallax.vue'
+import HomeSection from '~/components/HomeSection.vue'
 import AboutSection from '~/components/AboutSection.vue'
 import SessionsSection from '~/components/SessionsSection.vue'
 import EventsSection from '~/components/EventsSection.vue'
@@ -8,6 +9,7 @@ import ContactSection from '~/components/ContactSection.vue'
 
 <template>
     <!-- <BackgroundParallax> -->
+        <HomeSection />
         <AboutSection />
         <SessionsSection />
         <EventsSection />
